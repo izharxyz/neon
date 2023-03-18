dbcb0cd1467365386e79497fb762c4c2fc0d5660
zzedddd <melegend.forever@protonmail.com>
2023-03-19 00:21:13 +0530

    design layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
     import "../app.css";
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
   
-<slot />
+<div class="mx-2 md:mx-auto max-w-3xl md:max-w-5xl lg:max-w-7xl my-5 md:my-8 lg:my-10">
+    <div class="container mx-auto">
+        <Header/>
+            <div class="my-10 md:my-12 lg:mx-24">
+                <slot />
+            </div>
+        <Footer/>
+    </div>
+</div>
