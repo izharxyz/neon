4a5ca64da557148573671d4722d493774ae4e66d
zzedddd <melegend.forever@protonmail.com>
2023-03-22 00:01:22 +0530

    create category component

---
+++ src/lib/sidebar/Categories.svelte
@@ -0,0 +1,9 @@
+<div class="card w-full border border-primary-content">
+    <div class="card-body">
+      <h2 class="card-title">Card title!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn">Buy Now</button>
+      </div>
+    </div>
+</div>
