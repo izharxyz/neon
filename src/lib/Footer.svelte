5066fdabc71f2c3d388e3ffbb1789d2cbe5ddced
zzedddd <melegend.forever@protonmail.com>
2023-03-24 16:43:41 +0530

    design footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,40 @@
-<div class="border border-primary-content">
-    footer
+<div class="grid grid-cols-1 md:grid-cols-3 p-5 md:p-20 border border-primary-content shadow-xl rounded-3xl gap-12">
+   <div class="col-span-1">
+        <div class="text-3xl font-bold tracking-wide text-primary">
+            LOGO
+        </div>
+        <p class="mt-2">
+            When an unknown prnoto sans took a galley and scrambled it to make specimen
+            book not only five When an unknown prnoto sans took a galley and scrambled it to five centurie.
+        </p>
+        <h3 class="font-bold mt-5 text-xl">Contact</h3>
+        <p>myop.asfuck@email.org</p>
+   </div>
+   <div class="col-span-1">
+        <h1 class="font-bold text-xl">Categories</h1>
+        <h3 class="mt-5">Web dev</h3>
+        <h3 class="mt-2">Mobile dev</h3>
+        <h3 class="mt-2">Linux</h3>
+   </div>
+   <div class="col-span-1">
+        <h1 class="font-bold text-xl">Newsletter</h1>
+        <p class="mt-5">Sign up to be first to receive the latest stories inspiring us, case studies, and industry news.</p>
+        <form action="" class="mt-5">
+            <label for="">
+                Enter your email address
+            </label> 
+            <div class="mt-5 relative">
+                <input type="text" placeholder="username@site.com" class="input input-bordered w-full pr-16" /> 
+                <button class="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+            </div>
+        </form>
+   </div>
+   <div class="col-span-1 md:col-span-3 border-t border-primary-content flex flex-row justify-between pt-5 md:pt-20 md:mt-5">
+        <h3>View source on <span class="text-primary">Github</span></h3>
+        <div class="flex flex-row">
+            <p>SOCIAL</p>
+            <p class="ml-4">SOCIAL</p>
+            <p class="ml-4">SOCIAL</p>
+        </div>
+   </div>
 </div>
