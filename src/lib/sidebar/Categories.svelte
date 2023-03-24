83f3bcb5d354bc7ecc025da19861ceb15126c84f
zzedddd <melegend.forever@protonmail.com>
2023-03-24 22:47:26 +0530

    ui enhancement

--- src/lib/sidebar/Categories.svelte
+++ src/lib/sidebar/Categories.svelte
@@ -6,25 +6,25 @@
     <div class="card-body">
         <h2 class="card-title text-primary border-b border-primary-content pb-5">Categories</h2>
         
-        <div class="card w-full mt-5 bg-base-100 shadow-xl border border-primary-content p-3">
+        <div class="card w-full mt-5 bg-base-100 border border-primary-content p-3">
             <div class="flex flex-row items-center">
                 <img src={profile} alt="profile-pic" class="w-10 h-10 rounded-full"/>
                 <h3 class="ml-5">Web dev</h3>
             </div>
         </div>
-        <div class="card w-full mt-5 bg-base-100 shadow-xl border border-primary-content p-3">
+        <div class="card w-full mt-5 bg-base-100 border border-primary-content p-3">
             <div class="flex flex-row items-center">
                 <img src={profile} alt="profile-pic" class="w-10 h-10 rounded-full"/>
                 <h3 class="ml-5">Mobile dev</h3>
             </div>
         </div>
-        <div class="card w-full mt-5 bg-base-100 shadow-xl border border-primary-content p-3">
+        <div class="card w-full mt-5 bg-base-100 border border-primary-content p-3">
             <div class="flex flex-row items-center">
                 <img src={profile} alt="profile-pic" class="w-10 h-10 rounded-full"/>
                 <h3 class="ml-5">Linux</h3>
             </div>
         </div>
-        <div class="card w-full mt-5 bg-base-100 shadow-xl border border-primary-content p-3">
+        <div class="card w-full mt-5 bg-base-100 border border-primary-content p-3">
             <div class="flex flex-row items-center">
                 <img src={profile} alt="profile-pic" class="w-10 h-10 rounded-full"/>
                 <h3 class="ml-5">Blockchain</h3>
