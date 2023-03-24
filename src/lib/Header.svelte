089c2de6d955f707e45abf7a6b24ad3da8935fcd
zzedddd <melegend.forever@protonmail.com>
2023-03-24 17:40:49 +0530

    design header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,33 @@
-<div>
-    header
-</div>
+<script lang="ts">
+    export let profile = "/images/profile.jpg";
+</script>
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a href="/" class="btn btn-ghost normal-case text-3xl tracking-wide text-primary">LOGO</a>
+    </div>
+    <div class="flex-none gap-2">
+      <div class="form-control">
+        <input type="text" placeholder="Search" class="input input-bordered input-sm rounded-full px-5 h-10 w-20 md:w-40" />
+      </div>
+      <div class="dropdown dropdown-end">
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <label tabindex="0" class="btn btn-ghost btn-circle avatar" for="">
+          <div class="w-10 rounded-full">
+            <img src={profile} alt="profile-pic"/>
+          </div>
+        </label>
+
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+          <li>
+            <a href="/" class="justify-between">
+              Profile
+            </a>
+          </li>
+          <li><a href="/">Settings</a></li>
+          <li><a href="/">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
