d8f8b8ea63e3de78dc6ee03b9dec99df0d59b89a
zzedddd <melegend.forever@protonmail.com>
2023-03-28 00:02:22 +0530

    implement search

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let profile = "/images/profile.jpg";
+    let search: string;
 </script>
 
 <div class="navbar bg-base-100">
@@ -7,9 +8,14 @@
       <a href="/" class="normal-case text-3xl font-bold cursor-pointer tracking-wide text-primary hover:text-primary-focus transition duration-150">ZEDCODES</a>
     </div>
     <div class="flex-none gap-2">
-      <div class="form-control">
-        <input type="text" placeholder="Search" class="input input-bordered input-sm rounded-full px-3 md:px-5 h-10 w-20 md:w-40" />
-      </div>
+      <form class="form-control" action="/search/{search}">
+        <div class="input-group">
+          <input type="text" placeholder="Search…" class="input input-bordered w-24 md:w-52" bind:value={search}/>
+          <button class="btn btn-square">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </button>
+        </div>
+      </form>
       <div class="dropdown dropdown-end">
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <label tabindex="0" class="btn btn-ghost btn-circle avatar" for="">
---
+++ src/routes/search/[search]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+    export let data: PageData
+    $: ({ search } = data)
+</script>
+
+<h1 class="text-4xl text-primary"> Your search is: <span class="text-info">{search}</span> </h1>
