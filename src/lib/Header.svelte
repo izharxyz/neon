d6735334780798f6f60fcf0975f542d965563768
zzedddd <melegend.forever@protonmail.com>
2023-03-31 01:04:09 +0530

    add theme selector ui

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,7 +4,8 @@
     export let profile = "/images/profile.jpg";
     let search: string;
 
-    $: view = false;
+    $: showSearch = false;
+    $: showThemes = false;
 </script>
 
 <div class="navbar bg-base-100">
@@ -12,21 +13,23 @@
       <a href="/" class="normal-case text-3xl font-bold cursor-pointer tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">ZEDCODES</a>
     </div>
     <div class="flex-none gap-2">
-      <form class={view? "form-control" : "hidden form-control"} action="/search/{search}">
+      <form class={showSearch? "form-control" : "hidden form-control"} action="/search/{search}">
         <input type="text" placeholder="Search" class="input input-bordered input-secondary w-20 md:w-64 max-w-xs" bind:value={search}/>   
       </form>
-      <div>
-        <button class="h-7 w-7 mt-2" on:click={() => view = !view}><IoIosSearch/></button>
-      </div>
-      <form class="dropdown dropdown-end">
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <label tabindex="0" class="btn m-1" for="">Theme</label>
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 overflow-scroll">    
-          <li><button>valentine</button></li>
-          <li><button>retro</button></li>
+      <button class="btn" on:click={() => showSearch = !showSearch}>
+        <div class="h-7 w-7"><IoIosSearch/></div>
+      </button>
+      <button class="btn dropdown dropdown-end z-50" on:click={() => showThemes = !showThemes}>
+        <div class="h-7 w-7"><IoMdColorPalette/></div>
+        <ul class={showThemes? "dropdown-content menu p-5 shadow bg-base-100 rounded-box mt-4 border border-primary-content" : "hidden"}>
+            <li><button class="btn btn-ghost mt-2">Night</button></li>
+            <li><button class="btn btn-ghost mt-2">Snow</button></li>
+            <li><button class="btn btn-ghost">Valentine</button></li>
+            <li><button class="btn btn-ghost mt-2">Retro</button></li>
+            <li><button class="btn btn-ghost mt-2">Luxury</button></li>
+            <li><button class="btn btn-ghost mt-2">Cyberpunk</button></li>
         </ul>
-      </form>
+      </button>
       <div class="dropdown dropdown-end">
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <label tabindex="0" class="btn btn-ghost btn-circle avatar" for="">
