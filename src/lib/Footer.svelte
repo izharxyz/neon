3d437e8e853eb3095d1637b6df682f8a863514df
zzedddd <melegend.forever@protonmail.com>
2023-03-30 20:48:25 +0530

    added svelte-icons

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,9 @@
+<script>
+    import FaGithubSquare from 'svelte-icons/fa/FaGithubSquare.svelte'
+    import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte'
+    import FaTwitterSquare from 'svelte-icons/fa/FaTwitterSquare.svelte'
+</script>
+
 <div class="card grid grid-cols-1 md:grid-cols-3 p-5 md:p-20 border border-primary-content gap-12">
    <div class="col-span-1">
         <div class="text-3xl font-bold tracking-wide text-primary hover:text-primary-focus transition duration-150 cursor-pointer">
@@ -32,9 +38,17 @@
    <div class="col-span-1 md:col-span-3 border-t border-primary-content flex flex-row justify-between pt-5 md:pt-20 md:mt-5">
         <p>View source on <span class="text-primary hover:text-primary-focus transition duration-150 cursor-pointer">Github</span></p>
         <div class="flex flex-row">
-            <p>SOCIAL</p>
-            <p class="ml-4">SOCIAL</p>
-            <p class="ml-4">SOCIAL</p>
+            <div class="h-10 w-10 text-primary hover:text-primary-focus transition duration-200 cursor-pointer">
+                <a href="https://github.com/zzedddd">
+                    <FaGithubSquare/>
+                </a>
+            </div>
+            <div class="h-10 w-10 text-primary hover:text-primary-focus transition duration-200 cursor-pointer ml-4">
+                <FaLinkedin/>
+            </div>
+            <div class="h-10 w-10 text-primary hover:text-primary-focus transition duration-200 cursor-pointer ml-4">
+                <FaTwitterSquare/>
+            </div>
         </div>
    </div>
 </div>
