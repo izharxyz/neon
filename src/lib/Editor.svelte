22d11ff65f535d8cb01ffda6c7adac13721256c5
zzedddd <melegend.forever@protonmail.com>
2023-04-02 17:50:31 +0530

    remove svelte-icons and add iconify/svelte

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -2,22 +2,7 @@
   import StarterKit from "@tiptap/starter-kit";
   import { Editor } from "@tiptap/core";
   import { onMount } from "svelte";
-
-
-  import MdFormatBold from 'svelte-icons/md/MdFormatBold.svelte'
-  import MdFormatItalic from 'svelte-icons/md/MdFormatItalic.svelte'
-  import MdStrikethroughS from 'svelte-icons/md/MdStrikethroughS.svelte'
-  import MdCode from 'svelte-icons/md/MdCode.svelte'
-  import MdClear from 'svelte-icons/md/MdClear.svelte'
-  import MdBackspace from 'svelte-icons/md/MdBackspace.svelte'
-  import MdLocalParking from 'svelte-icons/md/MdLocalParking.svelte'
-  import MdFormatListBulleted from 'svelte-icons/md/MdFormatListBulleted.svelte'
-  import MdReorder from 'svelte-icons/md/MdReorder.svelte'
-  import MdFormatQuote from 'svelte-icons/md/MdFormatQuote.svelte'
-
-  import FaUndoAlt from 'svelte-icons/fa/FaUndoAlt.svelte'
-  import FaRedoAlt from 'svelte-icons/fa/FaRedoAlt.svelte'
-
+	import Icon from "@iconify/svelte";
 
   let element: any;
   let editor: any;
@@ -43,111 +28,122 @@
 </script>
 
 {#if editor}
-  <div class="border border-primary p-10 card">
-    <div>
+  <div class="border border-primary p-2 md:p-10 card">
+    <div class=" mx-auto">
       <button
         on:click={() => editor.chain().focus().toggleBold().run()}
         disabled={!editor.can().chain().focus().toggleBold().run()}
-        class={editor.isActive("bold") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("bold") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdFormatBold/>
-        </div>
+        <Icon icon="material-symbols:format-bold" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().toggleItalic().run()}
         disabled={!editor.can().chain().focus().toggleItalic().run()}
-        class={editor.isActive("italic") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("italic") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdFormatItalic/>
-        </div>
+        <Icon icon="material-symbols:format-italic" class="editor-icon"/>
       </button>
+      
       <button
         on:click={() => editor.chain().focus().toggleStrike().run()}
         disabled={!editor.can().chain().focus().toggleStrike().run()}
-        class={editor.isActive("strike") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("strike") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdStrikethroughS/>
-        </div>
+        <Icon icon="material-symbols:format-strikethrough" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().toggleCode().run()}
         disabled={!editor.can().chain().focus().toggleCode().run()}
-        class={editor.isActive("code") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("code") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdCode/>
-        </div>
+        <Icon icon="material-symbols:code" class="editor-icon"/>
       </button>
-      <button on:click={() => editor.chain().focus().unsetAllMarks().run()}>  
-        <div class="h-6 w-6">
-          <MdBackspace/>
-        </div> 
-      </button>
-      <button on:click={() => editor.chain().focus().clearNodes().run()}> 
-        <div class="h-6 w-6">
-          <MdClear/>
-        </div> 
-      </button>
+      <button on:click={() => editor.chain().focus().unsetAllMarks().run()} class="btn btn-xs"> <Icon icon="material-symbols:format-clear" class="editor-icon"/> </button>
+      <button on:click={() => editor.chain().focus().clearNodes().run()} class="btn btn-xs"> <Icon icon="ic:baseline-clear" class="editor-icon"/> </button>
       <button
         on:click={() => editor.chain().focus().setParagraph().run()}
-        class={editor.isActive("paragraph") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("paragraph") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdLocalParking/>
-        </div> 
+        <Icon icon="material-symbols:format-paragraph" class="editor-icon"/>
       </button>
-      
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        class={editor.isActive("heading", { level: 1 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h1" class="editor-icon"/>
+      </button>
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        class={editor.isActive("heading", { level: 2 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h2" class="editor-icon"/>
+      </button>
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        class={editor.isActive("heading", { level: 3 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h3" class="editor-icon"/>
+      </button>
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        class={editor.isActive("heading", { level: 4 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h4" class="editor-icon"/>
+      </button>
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        class={editor.isActive("heading", { level: 5 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h5" class="editor-icon"/>
+      </button>
+      <button
+        on:click={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        class={editor.isActive("heading", { level: 6 }) ? "btn btn-xs btn-secondary" : "btn btn-xs"}
+      >
+        <Icon icon="material-symbols:format-h6" class="editor-icon"/>
+      </button>
       <button
         on:click={() => editor.chain().focus().toggleBulletList().run()}
-        class={editor.isActive("bulletList") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("bulletList") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdFormatListBulleted/>
-        </div>
+        <Icon icon="material-symbols:format-list-bulleted-rounded" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().toggleOrderedList().run()}
-        class={editor.isActive("orderedList") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("orderedList") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdReorder/>
-        </div>
+        <Icon icon="material-symbols:format-list-numbered-rounded" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().toggleCodeBlock().run()}
-        class={editor.isActive("codeBlock") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("codeBlock") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdCode/>
-        </div>
+        <Icon icon="ri:code-s-slash-line" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().toggleBlockquote().run()}
-        class={editor.isActive("blockquote") ? "btn btn-xs btn-accent" : "btn btn-xs"}
+        class={editor.isActive("blockquote") ? "btn btn-xs btn-secondary" : "btn btn-xs"}
       >
-        <div class="h-6 w-6">
-          <MdFormatQuote/>
-        </div>
+        <Icon icon="material-symbols:format-quote" class="editor-icon"/>
       </button>
-      
+      <button on:click={() => editor.chain().focus().setHorizontalRule().run()} class="btn btn-xs">
+        <Icon icon="material-symbols:horizontal-rule" class="editor-icon"/>
+      </button>
+      <button on:click={() => editor.chain().focus().setHardBreak().run()} class="btn btn-xs"> <Icon icon="dashicons:editor-break" class="editor-icon"/> </button>
       <button
         on:click={() => editor.chain().focus().undo().run()}
         disabled={!editor.can().chain().focus().undo().run()}
+        class="btn btn-xs"
       >
-        <div class="h-5 w-5">
-          <FaUndoAlt/>
-        </div>
+        <Icon icon="material-symbols:undo" class="editor-icon"/>
       </button>
       <button
         on:click={() => editor.chain().focus().redo().run()}
         disabled={!editor.can().chain().focus().redo().run()}
+        class="btn btn-xs"
       >
-        <div class="h-5 w-5">
-          <FaRedoAlt/>
-        </div>
+        <Icon icon="material-symbols:redo" class="editor-icon"/>
       </button>
     </div>
   </div>
