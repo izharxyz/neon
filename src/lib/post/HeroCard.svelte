c56061d2865e3acfcb5748fbcf66fd72ebe823bb
zzedddd <melegend.forever@protonmail.com>
2023-04-14 01:12:01 +0530

    add default avatar

--- src/lib/post/HeroCard.svelte
+++ src/lib/post/HeroCard.svelte
@@ -5,7 +5,7 @@
     export let thumbnail = "/images/sky.jpg";
     export let excerpt = "An amazing post which is a must read!";
     export let author = "Zed";
-    // export let avatar = "/images/profile.jpg";
+    export let avatar: string | null = null;
     // export let time: number = 5;
 </script>
 
@@ -20,9 +20,13 @@
       <div class="card-actions justify-between">
         <div class="flex flex-row items-center">
             <div class="avatar">
+              {#if avatar}
+                <img src={avatar} alt="" class="h-10 w-10 rounded-full">
+              {:else}
                 <Icon icon="game-icons:bird-mask" class="icon border-2 border-primary rounded-full"/>
+              {/if}
             </div>
-            <h3 class="ml-4">
+            <h3 class="ml-4 text-base">
                 {author}
             </h3>
         </div> 
