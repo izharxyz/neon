e02a7c8f0fea4f91f68b470887f54302eeecf444
zzedddd <melegend.forever@protonmail.com>
2023-04-16 21:54:26 +0530

    setup types for lucia

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,6 +5,9 @@ import type { PrismaClient } from "@prisma/client";
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+			auth: import("lucia-auth").AuthRequest;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
@@ -12,6 +15,19 @@ declare global {
 	}
 	// eslint-disable-next-line no-var
 	var prisma: PrismaClient
+}
+
+/// <reference types="lucia-auth" />
+declare global {
+	namespace Lucia {
+		type Auth = import("$lib/lucia").Auth;
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		type UserAttributes = {
+			name: string;
+			username: string;
+			email: string;
+		};
+	}
 }
 
 export {};
