81d8f6672b8868b85eeed2a2bce597b6191e7f05
zzedddd <melegend.forever@protonmail.com>
2023-04-16 21:55:05 +0530

    create basic register form

---
+++ src/routes/auth/register/+page.svelte
@@ -0,0 +1,21 @@
+<!-- routes/signup/+page.svelte -->
+<script lang="ts">
+	import { enhance } from "$app/forms";
+</script>
+
+<div>
+	<h1>Create an account</h1>
+	<form method="POST" use:enhance>
+		<label for="name">name</label><br />
+		<input id="name" name="name" /><br />
+
+		<label for="username">username</label><br />
+		<input id="username" name="username" /><br />
+
+		<label for="email">email</label><br />
+		<input id="email" name="email" /><br />
+		<label for="password">Password</label><br />
+		<input type="password" id="password" name="password" /><br />
+		<input type="submit" value="Signup" class="btn btn-primary"/>
+	</form>
+</div>
