9bc8f9a67ae2d4349284de5bd328d57f953e19d3
zzedddd <melegend.forever@protonmail.com>
2023-04-18 15:45:24 +0530

    get user session data and pass it to header component

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+    const { user, session } = await locals.auth.validateUser()
+    return { user }
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+    import type { PageData } from './$types'
     import Icon from '@iconify/svelte';
     import "../app.css";
     import Header from "$lib/Header.svelte";
     import Footer from "$lib/Footer.svelte";
+
+    export let data: PageData;
 </script>
   
 <div class="mx-2 md:mx-auto max-w-3xl md:max-w-5xl lg:max-w-7xl my-5 md:my-8 lg:my-10">
     <div class="container mx-auto">
-        <Header/>
+        <Header user={data.user} />
             <div class="my-10 md:my-12 lg:mx-24">
                 <slot />
             </div>
