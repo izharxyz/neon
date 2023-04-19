d11c6057f1ac2b956c5023f76148cb162ea87fef
zzedddd <melegend.forever@protonmail.com>
2023-04-19 06:13:26 +0530

    render post

---
+++ src/routes/post/[postId]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import type { PageData } from "./$types";
+    export let data: PageData
+    $: ({ post } = data)
+</script>
+
+<div>
+    <h1 class="my-8 text-primary">{post.title}</h1>
+    <h3 class="mb-8">{post.excerpt} </h3>
+    <div class="w-full">
+        {@html post.content}
+    </div>
+    <a href="{$page.url}/edit/"><button class="my-8 btn btn-accent">Edit</button></a>
+</div>
