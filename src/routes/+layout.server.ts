fe1307900ce294e089195df9150082db3aaa4f3b
zzedddd <melegend.forever@protonmail.com>
2023-05-17 09:21:11 +0530

    delete svelte!

--- src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	const { user, session } = await locals.auth.validateUser();
-	return { user };
-};
--- src/routes/search/[search]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-	export let data: PageData;
-	$: ({ search } = data);
-</script>
-
-<h1 class="text-4xl text-primary text-center">
-	Your search is: <span class="text-info">{search}</span>
-</h1>
