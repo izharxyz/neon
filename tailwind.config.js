4ba4dd35dadba02d647a1d2a2876696982102f0f
zzedddd <melegend.forever@protonmail.com>
2023-06-05 12:29:19 +0530

    confing tailwind for blog app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./core/templates/core/*.html'],
+  content: ['./blog/templates/blog/*.html'],
   theme: {
     extend: {},
   },
