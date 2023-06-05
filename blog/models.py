7ff85f894a55698675e966256f65015dc51c0d5b
zzedddd <melegend.forever@protonmail.com>
2023-06-05 12:11:20 +0530

    create blog app

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=False, default='post title')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(null=False)
+    excerpt = models.CharField(
+        max_length=255, null=False, default='an amazing must read post')
+    thumbnail = models.ImageField(upload_to='images/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+
+    content = models.TextField()
+
+    views = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
