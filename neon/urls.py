21f02ec52157e696aaa4a1fcea2d6d2ffba1e847
zzedddd <melegend.forever@protonmail.com>
2023-06-05 11:11:20 +0530

    config whitenoise for serving static files

--- neon/urls.py
+++ neon/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
