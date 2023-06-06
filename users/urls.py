82624dd047840e072911d40b4af70d07885b037e
zzedddd <melegend.forever@protonmail.com>
2023-06-07 04:19:19 +0530

    create url and view for register

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import RegisterView, LoginView
 
 urlpatterns = [
-
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            return redirect('index')
+        else:
+            return render(request, 'users/register.html', {'form': form})
