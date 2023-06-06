5c5b9aa2d0fc451e1fedc78190bfa4dc35d7acd8
zzedddd <melegend.forever@protonmail.com>
2023-06-07 03:19:19 +0530

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./blog/templates/blog/*.html'],
+  content: [
+    './blog/templates/blog/*.html',
+    './users/templates/users/*.html'
+  ],
   theme: {
     extend: {},
   },
