ebe729dbce31490dc8f4085f631cf87a53cee06d
zzedddd <melegend.forever@protonmail.com>
2023-06-08 02:19:19 +0530

    create login route

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
 ]
