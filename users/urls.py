e76ddd6814befab3bf2e4fab06031cb328ef5378
zzedddd <melegend.forever@protonmail.com>
2023-06-18 10:15:13 +0530

    feat redirect if already logged in

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from .views import RegisterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(
+        template_name='users/login.html',
+        redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout'), name='logout')
 ]
