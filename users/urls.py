3e58ddd9bad76af85c3b4c4e73d3f7abda0babae
zzedddd <melegend.forever@protonmail.com>
2023-06-24 01:58:13 +0530

    create password reset route

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView, CheckUsernameExists
+from .views import RegisterView, CheckUsernameExists, PasswordResetView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,5 +9,6 @@ urlpatterns = [
         redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout'), name='logout'),
     path('check-username-exists/', CheckUsernameExists.as_view(),
-         name='check-username-exixts')
+         name='check-username-exixts'),
+    path('password-reset/', PasswordResetView.as_view(), name='password-reset')
 ]
