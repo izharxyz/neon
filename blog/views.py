8906da15aa031fe51cd9f4423f96c941d8fd357b
zzedddd <melegend.forever@protonmail.com>
2023-07-15 01:29:12 +0530

    added context to pass models to template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from blog.models import Post
 
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'blog/index.html')
+        featured_post = Post.objects.order_by('-views').first()
+        posts = Post.objects.all().exclude(id=featured_post.id)
+        return render(request, 'blog/index.html', {'featured_post': featured_post, 'posts': posts})
