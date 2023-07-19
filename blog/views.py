2632c4fe4f0b445227512a8a37e6f7d9e5eca11b
zzedddd <melegend.forever@protonmail.com>
2023-07-19 10:28:34 +0530

    fetching trending post from db and passing to template

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from blog.models import Post
 class IndexView(View):
     def get(self, request):
         featured_post = Post.objects.order_by('-views').first()
-        posts = Post.objects.all().exclude(id=featured_post.id)
-        return render(request, 'blog/index.html', {'featured_post': featured_post, 'posts': posts})
+        trending_posts = Post.objects.all().exclude(id=featured_post.id)[:5]
+        return render(request, 'blog/index.html', {'featured_post': featured_post, 'trending_posts': trending_posts})
