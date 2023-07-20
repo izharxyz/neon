2973219591dc9245f1e27250ce495f571d2607e8
zzedddd <melegend.forever@protonmail.com>
2023-07-21 01:42:34 +0530

    configured allowed hosts

--- neon/settings.py
+++ neon/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 
 # Application definition
