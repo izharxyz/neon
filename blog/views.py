c97e5f3c1184c8b60b2cd06916d03bc10ce0bef3
zzedddd <melegend.forever@protonmail.com>
2023-07-22 01:12:24 +0530

    added context of random posts

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,12 @@ class IndexView(View):
     def get(self, request):
         featured_post = Post.objects.order_by('-views').first()
         trending_posts = Post.objects.all().exclude(id=featured_post.id)[:5]
-        return render(request, 'blog/index.html', {'featured_post': featured_post, 'trending_posts': trending_posts})
+        random_posts = Post.objects.order_by(
+            '?').exclude(id=featured_post.id)[:9]
+
+        ctx = {
+            'featured_post': featured_post,
+            'trending_posts': trending_posts,
+            'random_posts': random_posts
+        }
+        return render(request, 'blog/index.html', ctx)
