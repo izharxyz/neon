27be5be249ac300d0080a386bc36d7ee1bf67e60
zzedddd <melegend.forever@protonmail.com>
2023-07-24 03:29:24 +0530

    added category in context

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views import View
-from blog.models import Post
+from blog.models import Category, Post
 
 
 class IndexView(View):
     def get(self, request):
         featured_post = Post.objects.order_by('-views').first()
-        trending_posts = Post.objects.all().exclude(id=featured_post.id)[:5]
+        trending_posts = Post.objects.all().exclude(id=featured_post.id)[:3]
         random_posts = Post.objects.order_by(
             '?').exclude(id=featured_post.id)[:9]
+        categories = Category.objects.order_by('name')
 
         ctx = {
             'featured_post': featured_post,
             'trending_posts': trending_posts,
-            'random_posts': random_posts
+            'random_posts': random_posts,
+            'categories': categories
         }
         return render(request, 'blog/index.html', ctx)
