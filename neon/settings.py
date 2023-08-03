ee870d125c127a827042eae47585e0ea38832a64
zzedddd <melegend.forever@protonmail.com>
2023-08-04 02:59:36 +0530

    configured login url

--- neon/settings.py
+++ neon/settings.py
@@ -130,5 +130,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
