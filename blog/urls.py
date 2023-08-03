15bb5ba4edfd7662e8223d22d038e25383307e50
zzedddd <melegend.forever@protonmail.com>
2023-08-04 03:07:36 +0530

    added blog-create route

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, PostDetailView
+from .views import IndexView, PostCreateView, PostDetailView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('blog/create/', PostCreateView.as_view(), name='post-create'),
     path('blog/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
