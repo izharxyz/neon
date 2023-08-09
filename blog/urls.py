a533aae871bd3446c422ddee7a47856e16aed1af
zzedddd <melegend.forever@protonmail.com>
2023-08-10 03:51:35 +0530

    created update route

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, PostCreateView, PostDetailView
+
+from .views import IndexView, PostCreateView, PostDetailView, PostUpdateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('blog/create/', PostCreateView.as_view(), name='post-create'),
     path('blog/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/update/<slug:slug>/', PostUpdateView.as_view(), name='post-update'),
 ]
