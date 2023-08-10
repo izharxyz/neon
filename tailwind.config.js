1c8dc1a0b07f502dafe764293748dc00b66edd6f
zzedddd <melegend.forever@protonmail.com>
2023-08-10 22:30:40 +0530

    changed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,6 @@ module.exports = {
     },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
     daisyui: {
-        themes: ["forest"],
+        themes: ["night"],
     },
 };
