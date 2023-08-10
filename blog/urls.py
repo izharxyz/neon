3edb0c8348fde59b1968ec152642d58d7d6abadb
zzedddd <melegend.forever@protonmail.com>
2023-08-10 22:28:50 +0530

    created route for post-delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import IndexView, PostCreateView, PostDetailView, PostUpdateView
+from .views import (IndexView, PostCreateView, PostDeleteView, PostDetailView,
+                    PostUpdateView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('blog/create/', PostCreateView.as_view(), name='post-create'),
     path('blog/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('blog/update/<slug:slug>/', PostUpdateView.as_view(), name='post-update'),
+    path('blog/delete/<slug:slug>/', PostDeleteView.as_view(), name='post-delete')
 ]
