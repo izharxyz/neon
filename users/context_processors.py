39910914cf5263f0c30a4a000294c5cd7bf430a9
zzedddd <melegend.forever@protonmail.com>
2023-08-11 03:12:01 +0530

    optimised custom context processor

--- users/context_processors.py
+++ users/context_processors.py
@@ -4,8 +4,8 @@ from users.models import Profile
 
 
 def profile_processor(request):
-    if request.user.is_authenticated:
+    if not request.user.is_authenticated:
+        return {'user_profile': None}
+    else:
         user_profile = get_object_or_404(Profile, user=request.user)
         return {'user_profile': user_profile}
-    else:
-        return {'user_profile': None}
