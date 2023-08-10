a9c27ffed3fb4406c3eabedc8d36cd6f12e3a842
zzedddd <melegend.forever@protonmail.com>
2023-08-11 03:00:19 +0530

    added custom context processor

--- neon/settings.py
+++ neon/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # custom context_processors
+                'users.context_processors.profile_processor',
             ],
         },
     },
